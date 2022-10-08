740ebe3d6a2f423ab95b756b80641073c8045f98
Estelle Z <zeukengestelle@gmail.com>
2022-10-08 20:09:36 +0100

    division des microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+set POSTGRES_USERNAME=udagram_db_3
+set POSTGRES_PASSWORD=udagram_db_3
+set POSTGRES_HOST=projet-3-udagram-db.cm9hxo93dpy2.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=udagram_db_3
+set AWS_BUCKET=udagram-3-s3
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
